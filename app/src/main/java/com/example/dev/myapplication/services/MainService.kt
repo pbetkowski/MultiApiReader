b64484f973dbad3a31867a91c7356e7b7158b534
pbetkowski <przbetkowski@gmail.com>
2019-01-26 14:52:59 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dev/myapplication/services/MainService.kt
@@ -0,0 +1,16 @@
+package com.example.dev.myapplication.services
+
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Headers
+
+interface MainService {
+
+//    @Headers("Accept: application/json", "Content-Type: application/json")
+//    @GET("http://api.nbp.pl/api/exchangerates/rates/A/USD?format-json")
+//    fun getUSD(): Observable<List<WMSClients>>
+//
+//    @Headers("Accept: application/json", "Content-Type: application/json")
+//    @GET("http://api.nbp.pl/api/exchangerates/rates/A/USD?format-json")
+//    fun getEUR(): Observable<List<WMSClients>>
+}
